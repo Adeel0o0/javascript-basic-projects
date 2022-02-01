44c887601e356dc146c7e2a9d4838c483d8c8460
Adeel <adeel.ahmed@goodlord.co>
2022-02-01 22:18:46 +0000

    modify button elements

---
+++ 02-counter/setup/app.js
@@ -0,0 +1,3 @@
+const buttonIncrease = document.getElementById("btn increase");
+const buttonDecrease = document.getElementById("btn decrease");
+const buttonReset = document.getElementById("btn reset");
