7c8735da4e998292ece4c4aa228b69b22060fc86
Adeel <adeel.ahmed@goodlord.co>
2022-02-03 14:14:08 +0000

    add count for each button

--- 02-counter/setup/app.js
+++ 02-counter/setup/app.js
@@ -1,3 +1,26 @@
-const buttonIncrease = document.getElementById("btn increase");
-const buttonDecrease = document.getElementById("btn decrease");
-const buttonReset = document.getElementById("btn reset");
+
+// set initial count
+let count = 0;
+
+// select value and buttons
+
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+        const styles = e.currentTarget.classList;
+        if(styles.contains("decrease")){
+            count--;
+        }
+        else if(styles.contains("increase")){
+            count++;
+        } 
+        else {
+            count = 0;
+        }
+        value.textContent = count;
+    });
+});
+
+
